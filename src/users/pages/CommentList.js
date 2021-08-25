1deb1e1fd9f8d3ac53a59793da20b592b3f5e139
pooreum <vnfma0218@naver.com>
2021-08-25 13:34:40 +0900

    comment pagination

--- src/users/pages/CommentList.js
+++ src/users/pages/CommentList.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { dbService } from '../../firebase';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 import { Button, Fade, makeStyles, Modal } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import Backdrop from '@material-ui/core/Backdrop';
 
 import './CommentList.css';
 import CommentItem from '../components/CommentItem';
+import { dbService } from '../../firebase';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -16,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     width: '1000px',
-    height: '500px',
+    height: '400px',
     overflowY: 'scroll',
     borderRadius: '10px',
     backgroundColor: theme.palette.background.paper,
@@ -29,6 +30,9 @@ const useStyles = makeStyles((theme) => ({
 export default function Comments(props) {
   const [comment, setComment] = useState('');
   const [commentList, setCommentList] = useState([]);
+  const [lastDoc, setLastDoc] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true);
   const classes = useStyles();
   const { open, handleClose } = props;
 
@@ -37,25 +41,74 @@ export default function Comments(props) {
   };
 
   useEffect(() => {
+    console.log('useEffect triggered');
+    setLoading(true);
     dbService
       .collection('rating')
       .where('productId', '==', props.id)
+      .orderBy('createdAt')
+      .limit(2)
       .get()
       .then((docs) => {
         let loadedComments = [];
         docs.forEach((doc) =>
           loadedComments.push({ ...doc.data(), id: doc.id })
         );
+        setLastDoc(docs.docs[docs.docs.length - 1]);
         setCommentList(loadedComments);
+        setLoading(false);
       });
   }, [props.id]);
+
+  const fetchNextComments = useCallback(() => {
+    setLoading(true);
+    dbService
+      .collection('rating')
+      .where('productId', '==', props.id)
+      .orderBy('createdAt')
+      .startAfter(lastDoc)
+      .limit(2)
+      .get()
+      .then((docs) => {
+        let loadedComments = [];
+        docs.forEach((doc) =>
+          loadedComments.push({ ...doc.data(), id: doc.id })
+        );
+        setLastDoc(docs.docs[docs.docs.length - 1]);
+        setCommentList((prev) => {
+          return [...prev, ...loadedComments];
+        });
+        setHasMore(!docs.empty);
+        setLoading(false);
+      });
+  }, [lastDoc, props.id]);
+
+  const observer = useRef();
+
+  const lastCommentElement = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      let options = {
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0,
+      };
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          fetchNextComments();
+        }
+      }, options);
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextComments, hasMore]
+  );
 
   return (
     <section className='comment-modal'>
       <Modal
         aria-labelledby='transition-modal-title'
         aria-describedby='transition-modal-description'
-        className={classes.modal}
+        className={`${classes.modal} commentModal`}
         open={open}
         onClose={handleClose}
         closeAfterTransition
@@ -89,16 +142,34 @@ export default function Comments(props) {
               </Button>
             </form>
             <ul className='user-commentList'>
-              {commentList.map((comment) => (
-                <CommentItem
-                  key={comment.id}
-                  createdAt={comment.createdAt}
-                  comment={comment.comment}
-                  avatar={comment.avatar}
-                  nickName={comment.nickName}
-                  rate={comment.rateValue}
-                />
-              ))}
+              {commentList.map((comment, index) => {
+                if (commentList.length === index + 1) {
+                  return (
+                    <div ref={lastCommentElement} id='lastComment'>
+                      <CommentItem
+                        key={comment.id}
+                        createdAt={comment.createdAt}
+                        comment={comment.comment}
+                        avatar={comment.avatar}
+                        nickName={comment.nickName}
+                        rate={comment.rateValue}
+                      />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <CommentItem
+                      key={comment.id}
+                      createdAt={comment.createdAt}
+                      comment={comment.comment}
+                      avatar={comment.avatar}
+                      nickName={comment.nickName}
+                      rate={comment.rateValue}
+                    />
+                  );
+                }
+              })}
+              {loading && <CircularProgress />}
             </ul>
           </div>
         </Fade>
