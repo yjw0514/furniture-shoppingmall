9f4a82f86dab39279e6e0cb1a304cea9116d9706
jw <jwjwhappy92@gmail.com>
2021-09-06 18:49:33 +0900

    cart paging error

--- src/shared/hooks/UseSliceProducts.js
+++ src/shared/hooks/UseSliceProducts.js
@@ -1,11 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useSliceProducts(initialLimit, originalProduct) {
+  const [currentProducts, setCurrentProducts] = useState([]);
   const [limit] = useState(initialLimit);
   const [currentPage, setCurrentPage] = useState(1);
   const lastIndex = currentPage * limit;
   const firstIndex = lastIndex - limit;
-  const currentProducts = originalProduct.slice(firstIndex, lastIndex);
+
+  useEffect(() => {
+    if (currentProducts.length === 0 && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  }, [currentProducts.length]);
+
+  useEffect(() => {
+    setCurrentProducts(originalProduct.slice(firstIndex, lastIndex));
+  }, [originalProduct, firstIndex, lastIndex]);
+
   const count = Math.ceil(originalProduct.length / initialLimit);
-  return { setCurrentPage, currentProducts, count };
+  return { setCurrentPage, currentProducts, count, setCurrentProducts };
 }
--- src/users/pages/ShoppingCart.js
+++ src/users/pages/ShoppingCart.js
@@ -16,10 +16,8 @@ export default function ShoppingCart() {
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
   const history = useHistory();
-  const { setCurrentPage, currentProducts, count } = useSliceProducts(
-    2,
-    cartProducts
-  );
+  const { setCurrentPage, currentProducts, setCurrentProducts, count } =
+    useSliceProducts(2, cartProducts);
 
   const cartRef = dbService.collection('cart').doc(currentUser.uid);
   const buyRef = dbService.doc(`/buy/${currentUser.uid}`);
@@ -35,10 +33,6 @@ export default function ShoppingCart() {
       }
     });
   }, [currentUser.uid]);
-
-  if (loading) {
-    return <CircularLoading />;
-  }
 
   const calcTotal = (products) => {
     let total = 0;
@@ -141,13 +135,14 @@ export default function ShoppingCart() {
   };
 
   const updateCurrentProduct = (productId) => {
-    console.log(currentProducts);
-    currentProducts.filter((el) => el.productId !== productId);
-    console.log(currentProducts.length);
-    if (currentProducts.length === 0) {
-      setCurrentPage((prev) => prev - 1);
-    }
+    setCurrentProducts(
+      currentProducts.filter((el) => el.productId !== productId)
+    );
   };
+
+  if (loading) {
+    return <CircularLoading />;
+  }
 
   let content;
   if (!loading && cartProducts.length === 0) {
